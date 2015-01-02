be6b3742a9872e22e12fc5e0a6953e41dcdd2a8f
Dimitrios J. Vergados <djvergad@gmail.com>
2015-01-02 21:40:52 +0200

    Made change to global

--- app/assets/javascripts/live.js
+++ app/assets/javascripts/live.js
@@ -5,6 +5,22 @@ var LiveClient = (function() {
             source.addEventListener('messages.keepalive', function(e) {
                 console.log("Client "+ i + ' received a message.');
             });
+        },
+        live_global: function(i) {
+            // We set source as global, otherwise we were left
+            // with sources remaining open after visiting internal
+            // pages
+            if (typeof source != "undefined" && source != null) {
+                console.log(source);
+                if (source.OPEN) {
+                    source.close();
+                    console.log("Closed source");
+                }
+            }
+            source = new EventSource("/stream/tick");
+            source.addEventListener('messages.keepalive', function(e) {
+                console.log("Client "+ i + ' received a message.');
+            });
         }
     };
 })();
