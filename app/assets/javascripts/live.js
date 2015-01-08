547c411ab1eb65294fbc2d78c27cade3b767aecd
Dimitrios J. Vergados <djvergad@gmail.com>
2015-01-08 12:28:59 +0200

    Testing

--- app/assets/javascripts/live.js
+++ app/assets/javascripts/live.js
@@ -1,23 +1,26 @@
 var LiveClient = (function() {
     return {
+/*
         live: function(i) {
             var source = new EventSource("/stream/tick");
             source.addEventListener('messages.keepalive', function(e) {
                 console.log("Client "+ i + ' received a message.');
             });
         },
+*/
+        abc: {},
         live_global: function(i) {
             // We set source as global, otherwise we were left
             // with sources remaining open after visiting internal
             // pages
-            if (typeof source != "undefined" && source != null) {
-                if (source.OPEN) {
-                    source.close();
+            if (typeof this.abc != "undefined" && this.abc != null) {
+                if (this.abc.OPEN) {
+                    this.abc.close();
                     console.log("Closed source");
                 }
             }
-            source = new EventSource("/stream/tick");
-            source.addEventListener('messages.keepalive', function(e) {
+            this.abc = new EventSource("/stream/tick");
+            this.abc.addEventListener('messages.keepalive', function(e) {
                 console.log("Client "+ i + ' received a message.');
             });
         }
